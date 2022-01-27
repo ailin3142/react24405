af714703efe986aff6ff3d503c5edf7023a40954
guillejf <85693115+guillejf@users.noreply.github.com>
2022-01-27 16:03:28 -0300

    clase 9 detalles sinimportancia

--- src/components/MuestraProducto.jsx
+++ src/components/MuestraProducto.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import ItemCount from "./ItemCount";
 
-export default function MuestraProducto({producto}) {
+export default function MuestraProducto({ producto }) {
 
     const [mostrarItemCount, setMostrarItemCount] = useState(true);
 
@@ -10,15 +10,15 @@ export default function MuestraProducto({producto}) {
         setMostrarItemCount(false);
     }
 
-  return (
-    <>
-        <div>{producto.id + " " + producto.nombre + ' ' + producto.stock}</div>
-        {
-            (mostrarItemCount)?
-                <ItemCount producto={producto} onAdd={onAdd} />
-            :
-            <button>Ve a terminar tu compra</button>
-        }        
-    </>
-  );
+    return (
+        <>
+            <div>{producto.id + " " + producto.nombre + ' ' + producto.stock}</div>
+            {
+                (mostrarItemCount) ?
+                    <ItemCount producto={producto} onAdd={onAdd} />
+                    :
+                    <button>Ve a terminar tu compra</button>
+            }
+        </>
+    );
 }
