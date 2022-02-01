d63260bbc25dfce3cf4218804c1bfa85395e428c
guillejf <85693115+guillejf@users.noreply.github.com>
2022-02-01 13:37:36 -0300

    inicio de clase 11

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import './App.css';
 import React, { useState, useEffect } from "react";
-import Comp1 from './components/Comp1';
-import ContextDemo from './components/ContextDemo';
+
 
 
 export default function App() {
     return (
     <>
-      <ContextDemo>
-
-        <Comp1 />
-        
-      </ContextDemo>
-
-
-
 
     </>
   );
