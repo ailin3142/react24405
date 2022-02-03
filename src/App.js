460c828a700864631623f4dc0882007369cb1cb3
guillejf <85693115+guillejf@users.noreply.github.com>
2022-02-03 11:33:02 -0300

    clase12 limpia

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import React, { useState, useEffect } from "react";
-import Hijo from "./components/Hijo";
 
 export default function App() {
 
-  const [estado, setEstado] = useState(0);
-
   useEffect(()=>{
-    console.log('PADRE RENDER ' + estado )
+    console.log('RENDER ')
   })
   
   return (
     <>
-      NUESTRA APP
-
-      <button onClick={()=>setEstado(Math.random())}>Cambiar el estado</button>
-      
-      <Hijo />
+    
   
     </>
   );
